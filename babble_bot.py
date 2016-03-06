3dea65c60d0e8c3e67dfc530bb068e85e7fbd870
Margot Ruthardt <margotruthardt@gmail.com>
2016-03-05 23:01:46 -0500

    sets up the bot to use a config.

--- babble_bot.py
+++ babble_bot.py
@@ -2,10 +2,14 @@ import telebot
 import re
 from mangle import Mangle
 import random
+import configparser
+
+config = configparser.ConfigParser()
+config.read("babble_bot.cfg")
 
 token = "218900293:AAHAt4y4KNYRqC1YPU0Q7YftJby4tvTrcEQ"
 
-bot = telebot.TeleBot(token)
+bot = telebot.TeleBot(config['telegram_bot_api']['telegram_token'])
 
 m = Mangle()
 
