e15326d63810d6c6d7174a8c72d05b10b01de800
Margot Ruthardt <margotruthardt@gmail.com>
2016-05-20 04:30:07 +0000

    commented out a logging line - it was causing errors. should fix it but not essential/

--- babble_bot.py
+++ babble_bot.py
@@ -17,7 +17,8 @@ freq = 0.1
 
 @bot.message_handler(func=lambda m: random.random() < freq)
 def echo_all(message):
-    print("[{}] Sending message to chat {} ({}).".format(datetime.now().time(), message.chat.title, message.chat.id))
+    # TODO getting an error here regarding utf characters. commenting it out because it's not needed.
+    #print("[{}] Sending message to chat {} ({}).".format(datetime.now().time(), message.chat.title, message.chat.id))
     bot.send_message(message.chat.id, m.mangle(message_text=message.text))
 		
 print("Bot started!")
